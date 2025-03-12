3a1f71c543a4ac3b151186badb5f8077c152c3aa
sebastian0023 <130592260+sebastian0023@users.noreply.github.com>
2025-03-12 08:33:29 -0600

    Create client.py

---
+++ Mongo/client.py
@@ -0,0 +1,35 @@
+import argparse
+import logging
+import os
+import requests
+
+
+# Set logger
+"""
+log = logging.getLogger()
+log.setLevel('INFO')
+handler = logging.FileHandler('books.log')
+handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
+log.addHandler(handler)
+
+# Read env vars related to API connection
+BOOKS_API_URL = os.getenv("BOOKS_API_URL", "http://localhost:8000")
+"""
+
+def prints(id):
+  pass
+
+def list_(id):
+  pass
+
+def get_data(id):
+  pass
+
+def update(id):
+  pass
+
+def delete(id):
+  pass
+
+def main():
+  pass
